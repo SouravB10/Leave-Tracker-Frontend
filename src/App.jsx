6124a3b96278ddf76df0193b4a460fb6c0220397
SouravB10 <souravbehuria10@gmail.com>
2025-03-25 11:38:48 +0530

    Sourav biatchhhhh

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-violet-500'>  
+      <h1 className='text-3xl font-bold text-white text-center p-4'>Leave Tracker</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
