324237d9a2ea2072c71a6af92362a74d8290ed90
SouravB10 <souravbehuria10@gmail.com>
2025-03-25 12:19:50 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import NavBar from './Screens/NavBar'
 
 function App() {
 
   return (
     <>
-    <div className='bg-violet-500'>  
-      <h1 className='text-3xl font-bold text-white text-center p-4'>Leave Tracker</h1>
-    </div>
+    <NavBar/>
+      <div className='bg-purple-500'>
+        <h1 className='text-3xl font-bold text-white text-center p-4'>Leave Tracker</h1>
+      </div>
     </>
   )
 }
